1175c77f4d43bbdd4b56372c9379e2bbf9a17ee9
kuroshiron <twang@gray.plala.or.jp>
2013-11-07 01:12:33 +0900

    uuuu

--- uninstall.sh
+++ uninstall.sh
@@ -5,15 +5,4 @@ fi
 echo "This will uninstall git by removing /usr/local/git/**/*, /etc/paths.d/git, /etc/manpaths.d/git"
 printf "Type 'yes' if you sure you wish to continue: "
 read response
-if [ "$response" == "yes" ]; then
-  sudo rm -rf /usr/local/git/
-  sudo rm /etc/paths.d/git
-  sudo rm /etc/manpaths.d/git
-  pkgutil --packages | grep GitOSX.Installer | xargs -I {} sudo pkgutil --forget {}
-  echo "Uninstalled"
-else
-  echo "Aborted"
-  exit 1
-fi
-
-exit 0
+if [ "$response" == "
